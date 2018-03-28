4a110c22d2788c83ab17e7f61a9fdbbc34e1cd2e
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-03-28 11:28:52 +0300

    Process component destruction

--- src/core/component.ts
+++ src/core/component.ts
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 
+import * as events from "devextreme/events";
+
+const DX_REMOVE_EVENT = "dxremove";
+
 @Component({
   template: "<div/>"
 })
@@ -8,7 +12,12 @@ export default class DxComponent extends Vue {
     protected _instance: any;
 
     public mounted(): void {
-        this._instance = this._createWidget(this.$el, this.$options.propsData);
+        this._instance = this._createWidget(this.$el, this.$options.propsData as any);
+    }
+
+    public beforeDestroy(): void {
+        events.triggerHandler(this.$el, DX_REMOVE_EVENT);
+        this._instance.dispose();
     }
 
     protected _createWidget(element: HTMLElement, props: Record<string, any>): any {
