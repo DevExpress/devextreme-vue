eee72118a5cb6c5bd5c67942fa899192b4af33e0
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-03-29 19:12:45 +0300

    Add popup example (#7)

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,10 @@ module.exports = {
       rules: [
         {
             test: /\.vue$/,
-            loader: 'vue-loader'
+            loader: 'vue-loader',
+            options: {
+                esModule: true
+            }
         },
         {
           test: /\.js$/,
