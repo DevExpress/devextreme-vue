74e503abb3876aaa4a68aca050d518277f7c6f73
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-03-29 18:52:36 +0300

    Add two-way binding

--- src/core/component.ts
+++ src/core/component.ts
@@ -13,11 +13,23 @@ export default class DxComponent extends Vue {
 
     public mounted(): void {
         this._instance = this._createWidget(this.$el, this.$options.propsData as any);
+        this._propsWatcher();
+        this._instance.on("optionChanged", (args: any) => {
+            this.$emit("update:" + args.name, args.value);
+        });
     }
 
     public beforeDestroy(): void {
         events.triggerHandler(this.$el, DX_REMOVE_EVENT);
         this._instance.dispose();
+    }
+
+    protected _propsWatcher(): void {
+        Object.keys(this.$props).forEach((prop: string) => {
+            this.$watch(prop, (value) => {
+                this._instance.option(prop, value);
+            });
+        });
     }
 
     protected _createWidget(element: HTMLElement, props: Record<string, any>): any {
