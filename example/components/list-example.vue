546c3e96588a4ad815c83420cdc775c9637b96c0
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-03-30 15:02:19 +0300

    Add more examples (#15)

---
+++ example/components/list-example.vue
@@ -0,0 +1,30 @@
+<template>
+    <example-block title="dxList">
+        <list 
+            :items="listData"
+            :itemTemplate="itemTemplate"
+        />
+    </example-block>
+</template>
+
+<script>
+import ExampleBlock from "./example-block";
+import { orangesByDay } from "./data";
+import { List } from "../../src";
+
+
+export default {
+    components: {
+        ExampleBlock,
+        List
+    },
+    data: function() {
+        return {
+            listData: orangesByDay,
+            itemTemplate: function(data, index, element) {
+                element.textContent = data.day;
+            }
+        }
+    }
+}
+</script>
---
+++ example/components/scheduler-example.vue
@@ -0,0 +1,30 @@
+<template>
+    <example-block title="dxScheduler">
+        <scheduler 
+            currentView="week"
+            :dataSource="appointments"
+            :height="400"
+            :currentDate="new Date(2017, 4, 25)"
+            :startDayHour="9"
+        />
+    </example-block>
+</template>
+
+<script>
+import ExampleBlock from "./example-block";
+import { appointments } from "./data";
+import { Scheduler } from "../../src";
+
+
+export default {
+    components: {
+        ExampleBlock,
+        Scheduler
+    },
+    data: function() {
+        return {
+            appointments: appointments
+        }
+    }
+}
+</script>
