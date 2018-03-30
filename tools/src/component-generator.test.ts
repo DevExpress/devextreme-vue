4a298bbe3867ad38d68b6f4363dff205ad2458ac
Ilya Kharchenko <IlyaKhD@users.noreply.github.com>
2018-03-30 10:35:11 +0300

    Add v-model support (editors only) (#10)

--- tools/src/component-generator.test.ts
+++ tools/src/component-generator.test.ts
@@ -72,3 +72,41 @@ export { CLASS_NAME };
         })
     ).toBe(EXPECTED);
 });
+
+it("generates class with model", () => {
+    //#region EXPECTED
+    const EXPECTED = `
+import dxCLASS_NAME from "devextreme/DX/WIDGET/PATH";
+import BaseComponent from "BASE_COMPONENT_PATH";
+
+import Vue from "vue";
+import VueComponent from "vue-class-component";
+
+@VueComponent({
+    mixins: [BaseComponent],
+    props: undefined,
+    model: { prop: "value", event: "update:value" }
+})
+class CLASS_NAME extends Vue {
+
+  public get instance(): dxCLASS_NAME {
+    return this._instance;
+  }
+
+  protected _createWidget(element: HTMLElement, props: Record<string, any>): any {
+    return new dxCLASS_NAME(element, props);
+  }
+}
+export { CLASS_NAME };
+`.trimLeft();
+    //#endregion
+
+    expect(
+        generate({
+            name: "CLASS_NAME",
+            baseComponentPath: "BASE_COMPONENT_PATH",
+            dxExportPath: "DX/WIDGET/PATH",
+            isEditor: true
+        })
+    ).toBe(EXPECTED);
+});
