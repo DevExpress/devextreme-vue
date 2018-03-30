bb75ece18eac01aba471a0d33e010f7bab071b30
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-03-30 15:00:10 +0300

    Add more examples

--- example/App.vue
+++ example/App.vue
@@ -6,23 +6,35 @@
         <br/>
         <text-box-example/>
         <br/>
+        <list-example/>
+        <br/>
         <grid-example/>
+        <br/>
+        <scheduler-example/>
+        <br/>
+        <chart-example/>
     </div>
 </template>
 
 <script>
 
 import ButtonExample from "./components/button-example";
+import ListExample from "./components/list-example";
 import GridExample from "./components/grid-example";
 import PopupExample from "./components/popup-example";
 import TextBoxExample from "./components/text-box-example";
+import SchedulerExample from "./components/scheduler-example";
+import ChartExample from "./components/chart-example";
 
 export default {
     components: {
         ButtonExample,
         PopupExample,
         TextBoxExample,
-        GridExample
+        ListExample,
+        GridExample,
+        SchedulerExample,
+        ChartExample
     },
     name: "app"
 }
---
+++ example/components/chart-example.vue
@@ -0,0 +1,33 @@
+<template>
+    <example-block title="dxChart">
+        <chart 
+            :dataSource="chartData"
+            :series="series"
+        />
+    </example-block>
+</template>
+
+<script>
+import ExampleBlock from "./example-block";
+import { orangesByDay } from "./data";
+import { Chart } from "../../src";
+
+
+export default {
+    components: {
+        ExampleBlock,
+        Chart
+    },
+    data: function() {
+        return {
+            chartData: orangesByDay,
+            series: [{
+                argumentField: "day",
+                valueField: "oranges",
+                name: "My oranges",
+                type: "line"
+            }]
+        }
+    }
+}
+</script>
