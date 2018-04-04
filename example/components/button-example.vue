a5ba118435f5076b34afe9ce166dd29c7ba2e700
Ilya Kharchenko <IlyaKhD@users.noreply.github.com>
2018-04-04 22:56:50 +0300

    Update button example (#24)

--- example/components/button-example.vue
+++ example/components/button-example.vue
@@ -1,6 +1,7 @@
 <template>
     <example-block title="dxButton">
         <dx-button text="clik me"/>
+        <dx-button text="I'm colored" :elementAttr="{ style: 'background-color: #ffc' }"/>
     </example-block>
 </template>
 
