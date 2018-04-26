228955d8e8fca0f3cf22fd10e93ae8ccfa4863ca
Ilya <ilyakhd@users.noreply.github.com>
2018-04-26 20:42:21 +0300

    Update integration data model

--- tools/integration-data-model.ts
+++ tools/integration-data-model.ts
@@ -6,16 +6,37 @@ export interface IWidget {
   subscribableOptions: ISubscribableOption[];
   templates: string[];
 }
+
 export interface IOption {
   name: string;
   options: IOption[];
   types: ITypeDescriptor[];
 }
+
 export interface ITypeDescriptor {
   acceptableValues: string[];
   type: string;
-  typeParams: ITypeDescriptor[];
 }
+
+export interface IArrayDescriptor extends ITypeDescriptor {
+  itemTypes: ITypeDescriptor[];
+}
+
+export interface IFunctionDescriptor extends ITypeDescriptor {
+  params: {
+    name: string;
+    types: ITypeDescriptor[];
+  }[];
+  returnValueType: ITypeDescriptor;
+}
+
+export interface IObjectDescriptor extends ITypeDescriptor {
+  fields: {
+    name: string;
+    types: ITypeDescriptor[];
+  }[];
+}
+
 export interface ISubscribableOption {
   name: string;
   type: string;
