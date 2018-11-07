e29a5f67bd99847fa33b792253f9456f338ee795
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-11-07 17:46:38 +0300

    Fix list hanging on selectionMode change (#191)

--- src/core/configuration.ts
+++ src/core/configuration.ts
@@ -1,4 +1,5 @@
 import { Vue } from "vue/types/vue";
+import { isEqual } from "./helpers";
 
 type UpdateFunc = (name: string, value: any) => void;
 
@@ -167,7 +168,9 @@ function subscribeOnUpdates(config: Configuration, vueInstance: Pick<Vue, "$emit
         } else if (args.fullName !== args.name) {
             optionValue = args.component.option(optionName);
         }
-        vueInstance.$emit("update:" + optionName, optionValue);
+        if (!isEqual(args.value, args.previousValue)) {
+            vueInstance.$emit("update:" + optionName, optionValue);
+        }
     };
 }
 
