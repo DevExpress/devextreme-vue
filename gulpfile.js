53fd373c540c3828123d743f11aa556f69a51c6e
Ilya Kharchenko <IlyaKhD@users.noreply.github.com>
2019-02-05 17:19:08 +0300

    Update to gulp 4 (#237)

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const shell = require('gulp-shell');
 const header = require('gulp-header');
 const ts = require('gulp-typescript');
 const tslint = require("gulp-tslint");
-const runSequence = require('run-sequence');
 
 const config = require('./build.config');
 
@@ -32,17 +31,6 @@ const
   NPM_BUILD = 'npm.build',
   NPM_PACK = 'npm.pack';
 
-gulp.task(GENERATE, (done) =>
-  runSequence(
-    CLEAN,
-    [OLD_OUTPUTDIR_CREATE, GEN_COMPILE],
-    GEN_RUN,
-    done
-  )
-);
-
-gulp.task(CLEAN, [OUTPUTDIR_CLEAN, GEN_CLEAN, NPM_CLEAN]);
-
 gulp.task(OUTPUTDIR_CLEAN, (c) =>
   del([`${config.generatedComponentsDir}\\*`, `!${config.coreComponentsDir}`], c)
 );
@@ -51,18 +39,24 @@ gulp.task(GEN_CLEAN, (c) =>
   del([config.generator.binDir], c)
 );
 
+gulp.task(NPM_CLEAN, (c) =>
+  del(config.npm.dist, c)
+);
+
+gulp.task(CLEAN, gulp.parallel(OUTPUTDIR_CLEAN, GEN_CLEAN, NPM_CLEAN));
+
 gulp.task(OLD_OUTPUTDIR_CREATE, (done) =>
   mkdir(config.oldComponentsDir, {}, done)
 );
 
-gulp.task(GEN_COMPILE, [GEN_CLEAN], () =>
+gulp.task(GEN_COMPILE, gulp.series(GEN_CLEAN, () =>
   gulp.src([config.generator.src, `!**/*.test.ts`])
     .pipe(ts({
       'target': 'es6',
       'module': 'commonjs'
     }))
     .pipe(gulp.dest(config.generator.binDir))
-);
+));
 
 gulp.task(GEN_RUN, (done) => {
   const generateSync = require(`${config.generator.binDir}generator.js`).default;
@@ -81,66 +75,74 @@ gulp.task(GEN_RUN, (done) => {
   done();
 });
 
+gulp.task(GENERATE, gulp.series(
+  gulp.parallel(OLD_OUTPUTDIR_CREATE, GEN_COMPILE),
+  GEN_RUN
+));
 
-gulp.task(NPM_CLEAN, (c) =>
-  del(config.npm.dist, c)
+gulp.task(NPM_PACKAGE,
+  () => gulp.src(config.npm.package).pipe(gulp.dest(config.npm.dist))
 );
 
-gulp.task(NPM_PACKAGE, [NPM_CLEAN], () =>
-  gulp.src(config.npm.package)
-    .pipe(gulp.dest(config.npm.dist))
-)
+gulp.task(NPM_LICENSE,
+  () => gulp.src(config.npm.license).pipe(gulp.dest(config.npm.dist))
+);
 
-gulp.task(NPM_LICENSE, [NPM_CLEAN], () =>
-  gulp.src(config.npm.license)
-    .pipe(gulp.dest(config.npm.dist))
-)
+gulp.task(NPM_README,
+  () => gulp.src(config.npm.readme).pipe(gulp.dest(config.npm.dist))
+);
 
-gulp.task(NPM_README, [NPM_CLEAN], () =>
-  gulp.src(config.npm.readme)
-    .pipe(gulp.dest(config.npm.dist))
-)
+gulp.task(NPM_BUILD, gulp.series(
+  CLEAN,
+  gulp.parallel(NPM_LICENSE, NPM_PACKAGE, NPM_README),
+  GENERATE,
+  () => {
+    return gulp.src([
+        config.src,
+        "!" + config.testSrc
+      ])
+      .pipe(ts('tsconfig.json'))
+      .pipe(gulp.dest(config.npm.dist))
+  }
+));
 
-gulp.task(NPM_BUILD, [NPM_LICENSE, NPM_PACKAGE, NPM_README, GENERATE], () => {
-  return gulp.src([
-      config.src,
-      "!" + config.testSrc
-    ])
-    .pipe(ts('tsconfig.json'))
-    .pipe(gulp.dest(config.npm.dist))
-});
+gulp.task(NPM_BUILD_WITH_HEADERS, gulp.series(
+  NPM_BUILD,
+  () => {
+    const pkg = require('./package.json'),
+        now = new Date(),
+        data = {
+            pkg: pkg,
+            date: now.toDateString(),
+            year: now.getFullYear()
+        };
 
-gulp.task(NPM_BUILD_WITH_HEADERS, [NPM_BUILD], function() {
-  const pkg = require('./package.json'),
-      now = new Date(),
-      data = {
-          pkg: pkg,
-          date: now.toDateString(),
-          year: now.getFullYear()
-      };
+    var banner = [
+        '/*!',
+        ' * <%= pkg.name %>',
+        ' * Version: <%= pkg.version %>',
+        ' * Build date: <%= date %>',
+        ' *',
+        ' * Copyright (c) 2012 - <%= year %> Developer Express Inc. ALL RIGHTS RESERVED',
+        ' *',
+        ' * This software may be modified and distributed under the terms',
+        ' * of the MIT license. See the LICENSE file in the root of the project for details.',
+        ' *',
+        ' * https://github.com/DevExpress/devextreme-vue',
+        ' */',
+        '\n'
+        ].join('\n');
 
-  var banner = [
-      '/*!',
-      ' * <%= pkg.name %>',
-      ' * Version: <%= pkg.version %>',
-      ' * Build date: <%= date %>',
-      ' *',
-      ' * Copyright (c) 2012 - <%= year %> Developer Express Inc. ALL RIGHTS RESERVED',
-      ' *',
-      ' * This software may be modified and distributed under the terms',
-      ' * of the MIT license. See the LICENSE file in the root of the project for details.',
-      ' *',
-      ' * https://github.com/DevExpress/devextreme-vue',
-      ' */',
-      '\n'
-      ].join('\n');
+    return gulp.src(path.join(config.npm.dist, config.npm.pattern))
+        .pipe(header(banner, data))
+        .pipe(gulp.dest(config.npm.dist));
+  }
+));
 
-  return gulp.src(path.join(config.npm.dist, config.npm.pattern))
-      .pipe(header(banner, data))
-      .pipe(gulp.dest(config.npm.dist));
-});
-
-gulp.task(NPM_PACK, [NPM_BUILD_WITH_HEADERS], shell.task(['npm pack'], { cwd: config.npm.dist }));
+gulp.task(NPM_PACK, gulp.series(
+  NPM_BUILD_WITH_HEADERS,
+  shell.task(['npm pack'], { cwd: config.npm.dist })
+));
 
 gulp.task(LINT, () => {
   return gulp.src([config.src, config.generator.src])
@@ -148,4 +150,4 @@ gulp.task(LINT, () => {
         formatter: "verbose"
     }))
     .pipe(tslint.report())
-})
+});
