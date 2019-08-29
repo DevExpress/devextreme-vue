35d8e61b4328f9289968c69cd5beccf9951f042f
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2019-08-29 17:27:14 +0300

    Fix lint

--- src/core/configuration-component.ts
+++ src/core/configuration-component.ts
@@ -22,12 +22,12 @@ interface IConfigurable extends IConfigurationOwner {
 const DxConfiguration: VueConstructor = Vue.extend({
 
     beforeMount() {
-        const internalChanges = {};
+        const innerChanges = {};
         const config = (this.$vnode.componentOptions as IConfigurable).$_config;
 
         config.init(Object.keys(this.$props));
-        bindOptionWatchers(config, this, internalChanges);
-        subscribeOnUpdates(config, this, internalChanges);
+        bindOptionWatchers(config, this, innerChanges);
+        subscribeOnUpdates(config, this, innerChanges);
     },
 
     mounted() {
