cda73309cb0ce2bf89c173e7cc5a5fb2d830ea33
Ivan Blinov <ivanblinov2015@mail.ru>
2020-10-23 14:52:39 +0300

    removed deprecated Ioption to Option, changed test

--- tools/src/component-generator.test.ts
+++ tools/src/component-generator.test.ts
@@ -1,8 +1,8 @@
 import generate, { renderProps } from "./component-generator";
 
 it("generates", () => {
-    //#region EXPECTED
-    const EXPECTED = `
+  //#region EXPECTED
+  const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 
@@ -25,30 +25,30 @@ export {
   COMPONENT
 };
 `.trimLeft();
-    //#endregion
+  //#endregion
 
-    expect(
-        generate({
-            name: "COMPONENT",
-            widgetComponent: {
-              name: "WIDGET",
-              path: "DX/WIDGET/PATH"
-            },
-            configComponent: {
-                name: "CONFIG_COMPONENT",
-                path: "./CONFIG_COMPONENT_PATH"
-            },
-            baseComponent: {
-                name: "BASE_COMPONENT",
-                path: "./BASE_COMPONENT_PATH"
-            }
-        })
-    ).toBe(EXPECTED);
+  expect(
+    generate({
+      name: "COMPONENT",
+      widgetComponent: {
+        name: "WIDGET",
+        path: "DX/WIDGET/PATH"
+      },
+      configComponent: {
+        name: "CONFIG_COMPONENT",
+        path: "./CONFIG_COMPONENT_PATH"
+      },
+      baseComponent: {
+        name: "BASE_COMPONENT",
+        path: "./BASE_COMPONENT_PATH"
+      }
+    })
+  ).toBe(EXPECTED);
 });
 
 it("generates component with model", () => {
-    //#region EXPECTED
-    const EXPECTED = `
+  //#region EXPECTED
+  const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 
@@ -72,35 +72,35 @@ export {
   COMPONENT
 };
 `.trimLeft();
-    //#endregion
+  //#endregion
 
-    expect(
-        generate({
-            name: "COMPONENT",
-            widgetComponent: {
-              name: "WIDGET",
-              path: "DX/WIDGET/PATH"
-            },
-            baseComponent: {
-                name: "BASE_COMPONENT",
-                path: "./BASE_COMPONENT_PATH"
-            },
-            configComponent: {
-                name: "CONFIG_COMPONENT",
-                path: "./CONFIG_COMPONENT_PATH"
-            },
-            hasModel: true
-        })
-    ).toBe(EXPECTED);
+  expect(
+    generate({
+      name: "COMPONENT",
+      widgetComponent: {
+        name: "WIDGET",
+        path: "DX/WIDGET/PATH"
+      },
+      baseComponent: {
+        name: "BASE_COMPONENT",
+        path: "./BASE_COMPONENT_PATH"
+      },
+      configComponent: {
+        name: "CONFIG_COMPONENT",
+        path: "./CONFIG_COMPONENT_PATH"
+      },
+      hasModel: true
+    })
+  ).toBe(EXPECTED);
 });
 
 it("generates option", () => {
-    //#region EXPECTED
-    const EXPECTED = `
-import WIDGET, { IOptions } from "devextreme/DX/WIDGET/PATH";
+  //#region EXPECTED
+  const EXPECTED = `
+import WIDGET, { Options } from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 
-type AccessibleOptions = Pick<IOptions,
+type AccessibleOptions = Pick<Options,
   "PROP"
 >;
 
@@ -131,31 +131,31 @@ export {
   COMPONENT
 };
 `.trimLeft();
-    //#endregion
+  //#endregion
 
-    expect(
-        generate({
-            name: "COMPONENT",
-            widgetComponent: {
-              name: "WIDGET",
-              path: "DX/WIDGET/PATH"
-            },
-            baseComponent: {
-                name: "BASE_COMPONENT",
-                path: "./BASE_COMPONENT_PATH"
-            },
-            configComponent: {
-                name: "CONFIG_COMPONENT",
-                path: "./CONFIG_COMPONENT_PATH"
-            },
-            props: [{ name: "PROP" }]
-        })
-    ).toBe(EXPECTED);
+  expect(
+    generate({
+      name: "COMPONENT",
+      widgetComponent: {
+        name: "WIDGET",
+        path: "DX/WIDGET/PATH"
+      },
+      baseComponent: {
+        name: "BASE_COMPONENT",
+        path: "./BASE_COMPONENT_PATH"
+      },
+      configComponent: {
+        name: "CONFIG_COMPONENT",
+        path: "./CONFIG_COMPONENT_PATH"
+      },
+      props: [{ name: "PROP" }]
+    })
+  ).toBe(EXPECTED);
 });
 
 it("generates nested option component", () => {
-    //#region EXPECTED
-    const EXPECTED = `
+  //#region EXPECTED
+  const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 import { CONFIG_COMPONENT } from "./CONFIG_COMPONENT_PATH";
@@ -192,40 +192,40 @@ export {
   NESTED_COMPONENT
 };
 `.trimLeft();
-        //#endregion
+  //#endregion
 
-    expect(
-        generate({
-            name: "COMPONENT",
-            widgetComponent: {
-              name: "WIDGET",
-              path: "DX/WIDGET/PATH"
-            },
-            baseComponent: {
-                name: "BASE_COMPONENT",
-                path: "./BASE_COMPONENT_PATH"
-            },
-            configComponent: {
-                name: "CONFIG_COMPONENT",
-                path: "./CONFIG_COMPONENT_PATH"
-            },
-            nestedComponents: [
-              {
-                name: "NESTED_COMPONENT",
-                optionName: "NESTED_OPTION_NAME",
-                props: [
-                  { name: "PROP" }
-                ],
-                isCollectionItem: false
-              }
-            ]
-        })
-    ).toBe(EXPECTED);
+  expect(
+    generate({
+      name: "COMPONENT",
+      widgetComponent: {
+        name: "WIDGET",
+        path: "DX/WIDGET/PATH"
+      },
+      baseComponent: {
+        name: "BASE_COMPONENT",
+        path: "./BASE_COMPONENT_PATH"
+      },
+      configComponent: {
+        name: "CONFIG_COMPONENT",
+        path: "./CONFIG_COMPONENT_PATH"
+      },
+      nestedComponents: [
+        {
+          name: "NESTED_COMPONENT",
+          optionName: "NESTED_OPTION_NAME",
+          props: [
+            { name: "PROP" }
+          ],
+          isCollectionItem: false
+        }
+      ]
+    })
+  ).toBe(EXPECTED);
 });
 
 it("generates nested collection option component", () => {
-    //#region EXPECTED
-    const EXPECTED = `
+  //#region EXPECTED
+  const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 import { CONFIG_COMPONENT } from "./CONFIG_COMPONENT_PATH";
@@ -263,40 +263,40 @@ export {
   NESTED_COMPONENT
 };
 `.trimLeft();
-        //#endregion
+  //#endregion
 
-    expect(
-        generate({
-            name: "COMPONENT",
-            widgetComponent: {
-              name: "WIDGET",
-              path: "DX/WIDGET/PATH"
-            },
-            baseComponent: {
-                name: "BASE_COMPONENT",
-                path: "./BASE_COMPONENT_PATH"
-            },
-            configComponent: {
-                name: "CONFIG_COMPONENT",
-                path: "./CONFIG_COMPONENT_PATH"
-            },
-            nestedComponents: [
-              {
-                name: "NESTED_COMPONENT",
-                optionName: "NESTED_OPTION_NAME",
-                props: [
-                  { name: "PROP" }
-                ],
-                isCollectionItem: true
-              }
-            ]
-        })
-    ).toBe(EXPECTED);
+  expect(
+    generate({
+      name: "COMPONENT",
+      widgetComponent: {
+        name: "WIDGET",
+        path: "DX/WIDGET/PATH"
+      },
+      baseComponent: {
+        name: "BASE_COMPONENT",
+        path: "./BASE_COMPONENT_PATH"
+      },
+      configComponent: {
+        name: "CONFIG_COMPONENT",
+        path: "./CONFIG_COMPONENT_PATH"
+      },
+      nestedComponents: [
+        {
+          name: "NESTED_COMPONENT",
+          optionName: "NESTED_OPTION_NAME",
+          props: [
+            { name: "PROP" }
+          ],
+          isCollectionItem: true
+        }
+      ]
+    })
+  ).toBe(EXPECTED);
 });
 
 it("generates expectedChildren info", () => {
-    //#region EXPECTED
-    const EXPECTED = `
+  //#region EXPECTED
+  const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 import { CONFIG_COMPONENT } from "./CONFIG_COMPONENT_PATH";
@@ -341,91 +341,91 @@ export {
   NESTED_COMPONENT
 };
 `.trimLeft();
-        //#endregion
+  //#endregion
 
-    expect(
-        generate({
-            name: "COMPONENT",
-            widgetComponent: {
-              name: "WIDGET",
-              path: "DX/WIDGET/PATH"
-            },
-            baseComponent: {
-                name: "BASE_COMPONENT",
-                path: "./BASE_COMPONENT_PATH"
-            },
-            configComponent: {
-                name: "CONFIG_COMPONENT",
-                path: "./CONFIG_COMPONENT_PATH"
-            },
-            nestedComponents: [
-              {
-                name: "NESTED_COMPONENT",
-                optionName: "NESTED_OPTION_NAME",
-                props: [
-                  { name: "PROP" }
-                ],
-                isCollectionItem: false,
-                expectedChildren: {
-                  EXPECTED_3: { isCollectionItem: true, optionName: "ghi" },
-                  EXPECTED_4: { isCollectionItem: false, optionName: "jkl" }
-                }
-              }
-            ],
-            expectedChildren: {
-              EXPECTED_1: { isCollectionItem: true, optionName: "abc" },
-              EXPECTED_2: { isCollectionItem: false, optionName: "def" }
-            }
-        })
-    ).toBe(EXPECTED);
+  expect(
+    generate({
+      name: "COMPONENT",
+      widgetComponent: {
+        name: "WIDGET",
+        path: "DX/WIDGET/PATH"
+      },
+      baseComponent: {
+        name: "BASE_COMPONENT",
+        path: "./BASE_COMPONENT_PATH"
+      },
+      configComponent: {
+        name: "CONFIG_COMPONENT",
+        path: "./CONFIG_COMPONENT_PATH"
+      },
+      nestedComponents: [
+        {
+          name: "NESTED_COMPONENT",
+          optionName: "NESTED_OPTION_NAME",
+          props: [
+            { name: "PROP" }
+          ],
+          isCollectionItem: false,
+          expectedChildren: {
+            EXPECTED_3: { isCollectionItem: true, optionName: "ghi" },
+            EXPECTED_4: { isCollectionItem: false, optionName: "jkl" }
+          }
+        }
+      ],
+      expectedChildren: {
+        EXPECTED_1: { isCollectionItem: true, optionName: "abc" },
+        EXPECTED_2: { isCollectionItem: false, optionName: "def" }
+      }
+    })
+  ).toBe(EXPECTED);
 });
 
 describe("props generation", () => {
 
-    it("generates props in alphabetic order", () => {
-        //#region EXPECTED
-        const EXPECTED =
-        `    --PROP: {},` + `\n` +
-        `    a-PROP: {},` + `\n` +
-        `    B-PROP: {},` + `\n` +
-        `    b-PROP: {}`;
-        //#endregion
+  it("generates props in alphabetic order", () => {
+    //#region EXPECTED
+    const EXPECTED =
+      `    --PROP: {},` + `\n` +
+      `    a-PROP: {},` + `\n` +
+      `    B-PROP: {},` + `\n` +
+      `    b-PROP: {}`;
+    //#endregion
 
-        expect(
-          renderProps([
-            { name: "B-PROP" },
-            { name: "b-PROP" },
-            { name: "a-PROP" },
-            { name: "--PROP" }
-          ])
-        ).toBe(EXPECTED);
-    });
+    expect(
+      renderProps([
+        { name: "B-PROP" },
+        { name: "b-PROP" },
+        { name: "a-PROP" },
+        { name: "--PROP" }
+      ])
+    ).toBe(EXPECTED);
+  });
 
-    it("generates props without type", () => {
-        const EXPECTED =
-        `    PROP1: {}`;
+  it("generates props without type", () => {
+    const EXPECTED =
+      `    PROP1: {}`;
 
-        expect(
-            renderProps([{ name: "PROP1" }])
-        ).toBe(EXPECTED);
-    });
+    expect(
+      renderProps([{ name: "PROP1" }])
+    ).toBe(EXPECTED);
+  });
 
-    it("generates props with type", () => {
+  it("generates props with type", () => {
 
-      const EXPECTED =
-        `    PROP1: TYPE1,` + `\n` +
-        `    PROP2: [TYPE2, TYPE3]`;
+    const EXPECTED =
+      `    PROP1: TYPE1,` + `\n` +
+      `    PROP2: [TYPE2, TYPE3]`;
 
-      expect(
-          renderProps([{ name: "PROP1", types: ["TYPE1"] }, { name: "PROP2", types: ["TYPE2", "TYPE3"] }])
-      ).toBe(EXPECTED);
+    expect(
+      renderProps([{ name: "PROP1", types: ["TYPE1"] }, { name: "PROP2", types: ["TYPE2", "TYPE3"] }])
+    ).toBe(EXPECTED);
 
-    });
+  });
 
-    it("generates props with acceptable values", () => {
-        //#region EXPECTED
-        const EXPECTED =
-`    PROP1: {
+  it("generates props with acceptable values", () => {
+    //#region EXPECTED
+    const EXPECTED =
+      `    PROP1: {
       type: TYPE1,
       validator: (v) => typeof(v) !== "string" || [
         "VAL1",
@@ -439,52 +439,52 @@ describe("props generation", () => {
         "VAL2"
       ].indexOf(v) !== -1
     }`;
-        //#endregion
+    //#endregion
 
-        expect(
-            renderProps([
-                {
-                    name: "PROP1",
-                    types: ["TYPE1"],
-                    acceptableValues: [`"VAL1"`, `"VAL2"`]
-                },
-                {
-                    name: "PROP2",
-                    types: ["TYPE2"],
-                    acceptableValues: [`"VAL1"`, `"VAL2"`],
-                    acceptableValueType: "TYPE3"
-                }
-            ])
-        ).toBe(EXPECTED);
-    });
+    expect(
+      renderProps([
+        {
+          name: "PROP1",
+          types: ["TYPE1"],
+          acceptableValues: [`"VAL1"`, `"VAL2"`]
+        },
+        {
+          name: "PROP2",
+          types: ["TYPE2"],
+          acceptableValues: [`"VAL1"`, `"VAL2"`],
+          acceptableValueType: "TYPE3"
+        }
+      ])
+    ).toBe(EXPECTED);
+  });
 
-    it("renders props with array of acceptable values", () => {
-        //#region EXPECTED
-        const EXPECTED =
-`    PROP1: {
+  it("renders props with array of acceptable values", () => {
+    //#region EXPECTED
+    const EXPECTED =
+      `    PROP1: {
       type: TYPE1,
       validator: (v) => !Array.isArray(v) || v.filter(i => [
         "VAL1",
         "VAL2"
       ].indexOf(i) === -1).length === 0
     }`;
-        //#endregion
+    //#endregion
 
-        expect(
-            renderProps([
-                {
-                    name: "PROP1",
-                    types: ["TYPE1"],
-                    acceptableValues: [`"VAL1"`, `"VAL2"`],
-                    isArray: true
-                }
-            ])
-        ).toBe(EXPECTED);
-    });
+    expect(
+      renderProps([
+        {
+          name: "PROP1",
+          types: ["TYPE1"],
+          acceptableValues: [`"VAL1"`, `"VAL2"`],
+          isArray: true
+        }
+      ])
+    ).toBe(EXPECTED);
+  });
 
-    it("generates nested component with predefined value", () => {
-        //#region EXPECTED
-        const EXPECTED = `
+  it("generates nested component with predefined value", () => {
+    //#region EXPECTED
+    const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 import { CONFIG_COMPONENT } from "./CONFIG_COMPONENT_PATH";
@@ -524,43 +524,43 @@ export {
   NESTED_COMPONENT
 };
 `.trimLeft();
-        //#endregion
+    //#endregion
 
-        expect(
-            generate({
-                name: "COMPONENT",
-                widgetComponent: {
-                  name: "WIDGET",
-                  path: "DX/WIDGET/PATH"
-                },
-                baseComponent: {
-                    name: "BASE_COMPONENT",
-                    path: "./BASE_COMPONENT_PATH"
-                },
-                configComponent: {
-                    name: "CONFIG_COMPONENT",
-                    path: "./CONFIG_COMPONENT_PATH"
-                },
-                nestedComponents: [
-                  {
-                    name: "NESTED_COMPONENT",
-                    optionName: "NESTED_OPTION_NAME",
-                    props: [
-                      { name: "PROP" }
-                    ],
-                    isCollectionItem: false,
-                    predefinedProps: {
-                      PROP_1: "PREDEFINED_VALUE"
-                    }
-                  }
-                ]
-            })
-        ).toBe(EXPECTED);
-    });
+    expect(
+      generate({
+        name: "COMPONENT",
+        widgetComponent: {
+          name: "WIDGET",
+          path: "DX/WIDGET/PATH"
+        },
+        baseComponent: {
+          name: "BASE_COMPONENT",
+          path: "./BASE_COMPONENT_PATH"
+        },
+        configComponent: {
+          name: "CONFIG_COMPONENT",
+          path: "./CONFIG_COMPONENT_PATH"
+        },
+        nestedComponents: [
+          {
+            name: "NESTED_COMPONENT",
+            optionName: "NESTED_OPTION_NAME",
+            props: [
+              { name: "PROP" }
+            ],
+            isCollectionItem: false,
+            predefinedProps: {
+              PROP_1: "PREDEFINED_VALUE"
+            }
+          }
+        ]
+      })
+    ).toBe(EXPECTED);
+  });
 
-    it("generates nested component with several predefined values", () => {
-        //#region EXPECTED
-        const EXPECTED = `
+  it("generates nested component with several predefined values", () => {
+    //#region EXPECTED
+    const EXPECTED = `
 import WIDGET from "devextreme/DX/WIDGET/PATH";
 import { BASE_COMPONENT } from "./BASE_COMPONENT_PATH";
 import { CONFIG_COMPONENT } from "./CONFIG_COMPONENT_PATH";
@@ -601,38 +601,38 @@ export {
   NESTED_COMPONENT
 };
 `.trimLeft();
-        //#endregion
+    //#endregion
 
-        expect(
-            generate({
-                name: "COMPONENT",
-                widgetComponent: {
-                  name: "WIDGET",
-                  path: "DX/WIDGET/PATH"
-                },
-                baseComponent: {
-                    name: "BASE_COMPONENT",
-                    path: "./BASE_COMPONENT_PATH"
-                },
-                configComponent: {
-                    name: "CONFIG_COMPONENT",
-                    path: "./CONFIG_COMPONENT_PATH"
-                },
-                nestedComponents: [
-                  {
-                    name: "NESTED_COMPONENT",
-                    optionName: "NESTED_OPTION_NAME",
-                    props: [
-                      { name: "PROP" }
-                    ],
-                    isCollectionItem: false,
-                    predefinedProps: {
-                      PROP_1: "PREDEFINED_VALUE_1",
-                      PROP_2: "PREDEFINED_VALUE_2"
-                    }
-                  }
-                ]
-            })
-        ).toBe(EXPECTED);
-    });
+    expect(
+      generate({
+        name: "COMPONENT",
+        widgetComponent: {
+          name: "WIDGET",
+          path: "DX/WIDGET/PATH"
+        },
+        baseComponent: {
+          name: "BASE_COMPONENT",
+          path: "./BASE_COMPONENT_PATH"
+        },
+        configComponent: {
+          name: "CONFIG_COMPONENT",
+          path: "./CONFIG_COMPONENT_PATH"
+        },
+        nestedComponents: [
+          {
+            name: "NESTED_COMPONENT",
+            optionName: "NESTED_OPTION_NAME",
+            props: [
+              { name: "PROP" }
+            ],
+            isCollectionItem: false,
+            predefinedProps: {
+              PROP_1: "PREDEFINED_VALUE_1",
+              PROP_2: "PREDEFINED_VALUE_2"
+            }
+          }
+        ]
+      })
+    ).toBe(EXPECTED);
+  });
 });
