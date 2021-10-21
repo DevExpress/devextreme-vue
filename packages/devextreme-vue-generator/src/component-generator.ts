6d51f5b9691315461c8ea6f9b2d09ec9268ca33b
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2021-10-21 11:45:26 +0300

    Add an exception components for async templates

--- packages/devextreme-vue-generator/src/component-generator.ts
+++ packages/devextreme-vue-generator/src/component-generator.ts
@@ -65,13 +65,17 @@ function generateReExport(path: string, fileName: string): string {
     return renderReExport({ path, fileName });
 }
 
+export const USE_SYNC_TEMPLATES: Set<string> = new Set([
+    "DxScheduler"
+]);
+
 const renderReExport: (model: {path: string, fileName: string}) => string = createTempate(
 `/** @deprecated Use 'devextreme-vue/<#= it.fileName #>' file instead */\n` +
 `export * from "<#= it.path #>";\n` +
 `export { default } from "<#= it.path #>";\n`
 );
 
-function generate(component: IComponent, widgetsPackage: string = "devextreme"): string {
+function generate(component: IComponent, widgetsPackage: string = "devextreme", vueVersion: number = 3): string {
     const nestedComponents = component.nestedComponents
         ? component.nestedComponents.map(createNestedComponentModel)
         : undefined;
@@ -104,7 +108,9 @@ function generate(component: IComponent, widgetsPackage: string = "devextreme"):
         defaultExport: component.name,
         namedExports,
         expectedChildren: formatExpectedChildren(component.expectedChildren),
-        widgetsPackage
+        widgetsPackage,
+        templatesRenderAsynchronously: !USE_SYNC_TEMPLATES.has(component.name),
+        isVue3: vueVersion === 3
     };
 
     return renderComponent(componentModel);
@@ -158,6 +164,8 @@ const renderComponent: (model: {
     defaultExport: string;
     namedExports: string[];
     widgetsPackage: string;
+    templatesRenderAsynchronously: boolean;
+    isVue3: boolean;
 
 }) => string = createTempate(
 `import <#= it.widgetImport.name #><#? it.props #>, { Options }<#?#> from "<#= it.widgetsPackage #>/<#= it.widgetImport.path #>";\n` +
@@ -208,7 +216,9 @@ L1 + `},` +
 
 L1 + `beforeCreate() {` +
     L2 + `(this as any).$_WidgetClass = <#= it.widgetImport.name #>;` +
-
+    `<#? it.isVue3 #>` +
+    L2 + `(this as any).$_hasAsyncTemplate = <#= it.templatesRenderAsynchronously #>;` +
+    `<#?#>` +
     `<#? it.expectedChildren #>` +
     L2 + `(this as any).$_expectedChildren = {` +
     `<#~ it.expectedChildren : child #>` +
