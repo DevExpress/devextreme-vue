e8d97549df08903b11f3e9b7bccfd3f3011cdda0
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2021-10-21 11:46:29 +0300

    Fix lint

--- packages/devextreme-vue-generator/src/component-generator.test.ts
+++ packages/devextreme-vue-generator/src/component-generator.test.ts
@@ -440,9 +440,9 @@ describe("addintion info generation", () => {
   });
   afterEach(() => {
     USE_SYNC_TEMPLATES.delete("COMPONENT");
-  })
+  });
 
-    it("generates", () => {
+  it("generates", () => {
       //#region EXPECTED
       const EXPECTED = `
 import WIDGET from "widget-gackage-name/DX/WIDGET/PATH";
@@ -488,7 +488,7 @@ export {
           }, "widget-gackage-name", 3)
       ).toBe(EXPECTED);
   });
-})
+});
 
 describe("props generation", () => {
 
