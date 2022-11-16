0a3a42e44bc92b7bb2f9f129f38ef421c2b938e0
Alyar <>
2022-11-16 15:33:10 +0400

    Use sunc template for dxDataGrid

--- packages/devextreme-vue-generator/src/component-generator.ts
+++ packages/devextreme-vue-generator/src/component-generator.ts
@@ -67,7 +67,8 @@ function generateReExport(path: string, fileName: string): string {
 }
 
 export const USE_SYNC_TEMPLATES: Set<string> = new Set([
-    "DxScheduler"
+    "DxScheduler",
+    "DxDataGrid"
 ]);
 
 const renderReExport: (model: {path: string, fileName: string}) => string = createTempate(
