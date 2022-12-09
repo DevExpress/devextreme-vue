d2727f020d04c01320afe6dd688607e33d159019
iBat <git@bulychev.pro>
2022-12-09 10:53:35 +0400

    CI: get rid of lerna, update node/TS

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,7 +1,13 @@
 module.exports = {
   roots: ['<rootDir>'],
   transform: {
-    '^.+\\.(j|t)sx?$': 'ts-jest',
+    '.*': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        // tsconfig: 'jest.tsconfig.json'
+      },
+    ]
   },
   testURL: 'http://localhost',
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
